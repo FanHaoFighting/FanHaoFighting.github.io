932dd200e7b6ce4b7baaad7fa56045ba8dc856b9
fanhao <fanhaohf@163.com>
2019-07-29 21:55:19 +0800

    difference方法提交

--- miao/lodash/fanhaofighting-lodash.js
+++ miao/lodash/fanhaofighting-lodash.js
@@ -1,7 +1,20 @@
 var fanhaofighting = function () {
   return { compact, chunk }
 
+
+  
+  function difference(arr, ...values) {
+    return arr.filter(item => values.every(value => !value.includes(item)))
+  }
+
+
+  /**
+   * Creates an array with all falsey values removed. The values `false`, `null`,
+   * `0`, `""`, `undefined`, and `NaN` are falsey.
+   * @param {*} ary 
+   */
   function compact(ary) {
+    // filter返回布尔值, it中的null, undefined, ""会被返回false
     return ary.filter(it => it)
   }
 
