607d2977e0444f08f0f3af7d7273ead5956a7664
fanhao <fanhaohf@163.com>
2019-08-25 15:18:55 +0800

    若干方法提交

--- miao/lodash/fanhaofighting-lodash.js
+++ miao/lodash/fanhaofighting-lodash.js
@@ -176,7 +176,7 @@ var fanhaofighting = function () {
    */
   function differenceWith(arr, ...values) {
     let pridicate = values.pop()
-    return  arr.filter(item => values.every(value => !value.map(it => pridicate(it, item))))
+    return arr.filter(item => values.every(value => pridicate(value, item)))
   }
 
   /**
