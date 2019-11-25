5824ed1fb448ef976899f06e6b73e4c551660a4b
fanhao <fanhaohf@163.com>
2019-11-25 17:22:34 +0800

    groupBy方法

--- miao/lodash/fanhaofighting-lodash.js
+++ miao/lodash/fanhaofighting-lodash.js
@@ -12,9 +12,25 @@ var fanhaofighting = function () {
 
   }
 
-  // todo
-  function groupBy() {
-
+  /**
+   * 创建一个对象，key 是 iteratee 遍历 collection(集合) 中的每个元素返回的结果。 
+   * 分组值的顺序是由他们出现在 collection(集合) 中的顺序确定的。
+   * 每个键对应的值负责生成 key 的元素组成的数组。iteratee 调用 1 个参数： (value)。
+   * @param {(Array|Object)} collection 
+   * @param { (Array|Function|Object|string)} predicate 
+   */
+  function groupBy(collection, predicate) {
+    predicate = iteratee(predicate)
+    let map = {}
+    for (let key in Object.keys(collection)) {
+      let mapKey = predicate(collection[key])
+      if (mapKey) {
+        map[mapKey].push(collection[key])
+      } else {
+        map[mapKey] = []
+      }
+    }
+    return map
   }
 
   // todo
